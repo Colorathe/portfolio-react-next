965c259bc3cb263d7c19f163e5d41dce50c06100
Colorathe <joacovulcano@gmail.com>
2022-09-19 18:23:30 -0300

    fix bugs

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -10,8 +10,8 @@ const Hero = () => {
       <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
         <div>
           <h1 className='py-4 text-gray-300'>
-            Hey there, I &apos;m
-            <span className='text-[#5651e5]'>Joaquin Vulcano</span>
+            Hey there, I&apos;m
+            <span className='text-[#5651e5]'> Joaquin Vulcano</span>
           </h1>
           <h1 className='py-2 text-gray-300'>Front-End Developer</h1>
           <p className='py-4 text-gray-300 max-w-[70%] m-auto'>
